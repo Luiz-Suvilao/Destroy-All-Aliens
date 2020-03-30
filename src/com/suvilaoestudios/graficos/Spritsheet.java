787714eb4e439598a18f37a49e48fd61144f93af
Luiz-Suvilao <suvilao@gmail.com>
2020-03-30 18:34:39 -0300

    Classe que controla todas as sprites do jogo

---
+++ src/com/suvilaoestudios/graficos/Spritsheet.java
@@ -0,0 +1,26 @@
+package com.suvilaoestudios.graficos;
+
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+public class Spritsheet 
+{
+	private static  BufferedImage spritsheet;
+
+		public Spritsheet(String path)
+		{
+			try {
+				spritsheet = ImageIO.read(getClass().getResource(path));
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		public BufferedImage getSprite(int x, int y, int width, int height)
+		{
+			return spritsheet.getSubimage(x, y, width, height);
+			
+		}
+}
